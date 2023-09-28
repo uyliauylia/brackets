9e8a9772e6816f786d4b3fcd7685b74781c6e096
Yulia Smirnova <uyliauylia@yandex.ru>
2023-09-28 15:32:22 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let i = 0;
+  let result = str;
+
+  const parsedBracketsConfig = bracketsConfig.map((item) => {
+    return `${item[0]}${item[1]}`;
+  });
+
+  let mainLength = str.length;
+
+  while (result.length>i) {
+
+    parsedBracketsConfig.forEach((item) => {
+      result = result.replaceAll(item, '');
+      console.log(result);
+    });
+      if (result.length === mainLength ) {
+        return false;
+      }
+    mainLength = result.length;
+  }
+  return true;
 }
